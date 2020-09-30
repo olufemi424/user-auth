ae5d507b1688561af38115198e8e2a2354eb1276
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2020-09-30 01:03:38 -0500

    db environment

--- db/db.js
+++ db/db.js
@@ -2,15 +2,18 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
+  const dbUrl =
+    process.env.NODE_ENV !== `production`
+      ? `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.iyvq7.mongodb.net/${process.env.MONGO_DB_DEV}?retryWrites=true&w=majority`
+      : `mongodb+srv:/${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@af-proexprod.c0pmy.mongodb.net/${process.env.MONGO_DB_PROD}?retryWrites=true&w=majority`;
+
+  console.log(dbUrl);
   try {
-    const conn = await mongoose.connect(
-      `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.iyvq7.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true
-      }
-    );
+    const conn = await mongoose.connect(dbUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    });
 
     console.log(`DB connected: ${conn.connection.host}`);
   } catch (err) {
